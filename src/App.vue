9f96635d9100cd89e6070803f3e5f47ed282ab2f
Ömer Faruk DEMİREL <59699332+faruukdemirel@users.noreply.github.com>
2021-04-26 17:54:18 +0300

    up

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TheHeader />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import TheHeader from "@/components/TheHeader";
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <select 
+        class="form-select mt-10 block w-full border p-3 rounded "
+        @change="onChangeSelect()"
+         v-model="selected">
+            <option value="0">Select Country</option>
+            <option  v-for="country in countries" :key="country.ID" :value="country.ID">
+                {{country.Country}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CountrySelect',
+        props:['countries'],
+        data(){
+            return{
+                selected:0
+            }
+        },
+        methods:{
+            onChangeSelect(){
+                const country=this.countries.find((item)=>item.ID == this.selected)
+                this.$emit('get-country',country)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
